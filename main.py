205c5a53b866cbb23b327bb7733e8e56b210f1c1
Chris Meyers <chris.meyers.fsu@gmail.com>
2016-05-09 13:03:29 +0000

    creds moved out of main

--- main.py
+++ main.py
@@ -2,7 +2,25 @@
 from __future__ import division
 import spidev
 import time
+import requests
+import sys
 import RPi.GPIO as GPIO
+from datetime import datetime
+from datetime import timedelta
+from collections import deque
+from requests.exceptions import ConnectionError
+from requests.auth import HTTPBasicAuth
+import json
+import ConfigParser
+config = ConfigParser.ConfigParser()
+config.read('kegometer.cfg')
+
+username = config.get('tower', 'username', None)
+password = config.get('tower', 'password', None)
+job_template_id = config.get('tower', 'job_template_id', None)
+if not username or not password or not job_template_id:
+    print("Tower username and password not found")
+    sys.exit(0)
 
 spi = spidev.SpiDev(0, 0)
 #spi.max_speed_hz = 500000 # 1.2 MHz
@@ -15,11 +33,16 @@ def read_spi():
     return data
 
 class Scale:
+    v_history = None
+    v_average = 0
     v_prev = 0
     v_curr = 0
     v_last_reading = 0
     tick_count = 0
     READING_THRESHOLD = .1
+
+    def __init__(self):
+        self.v_history = deque(maxlen=20000)
 
     def get_v_diff(self):
         return abs(self.v_prev - self.v_curr)
@@ -30,14 +53,19 @@ class Scale:
         self.v_curr = v
         if abs(self.v_last_reading -v) > self.READING_THRESHOLD:
             self.v_last_reading = v
+            self.v_history.append(v)
             return (v, True)
         return (v, False)
 
     def tick_done(self):
         self.v_prev = self.v_curr
 
+    def calc_average(self):
+        return (float(sum(self.v_history)) / len(self.v_history))
+
 scale = Scale()
 if __name__ == '__main__':
+    next_time = datetime.now()
     while True:
         (v, new_reading) = scale.tick()
         if v == 0:
@@ -49,7 +77,22 @@ if __name__ == '__main__':
         equals = '='*int(v)
         if scale.tick_count % 100 == 0:
             print("%s, %s" % (v, equals))
+
         
+        if datetime.now() >= next_time:
+            payload = {
+                'extra_vars': {
+                    'beer_weight': v
+                }
+            }
+            try:
+                headers = {'content-type': 'application/json'}
+                r = requests.post('https://tower.testing.ansible.com/api/v1/job_templates/%s/launch/' % job_template_id, data=json.dumps(payload), auth=HTTPBasicAuth(username, password), verify=False, headers=headers)
+                print("%s" % r)
+            except ConnectionError as e:
+                print("Error %s" % e)
+
+            next_time = datetime.now() + timedelta(hours=1)
 
         scale.tick_done()
 
